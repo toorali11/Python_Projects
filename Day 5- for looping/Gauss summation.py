b0eb78845df310c689ba17b45d637284ac9787fa
Toor Ali <toorali11@gmail.com>
2025-11-29 13:33:39 +0100

    Create Gauss summation.py

---
+++ Day 5- for looping/Gauss summation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 29 10:52:25 2025
+
+@author: toora
+"""
+# Gauss’s summation (n(n+1))/2
+
+a=0
+for number in range (0,101):
+    a+=number
+print(a)
