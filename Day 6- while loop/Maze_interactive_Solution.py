4b49710a0651044ed032a22cc67287ca4a633993
Toor Ali <toorali11@gmail.com>
2025-11-29 19:41:37 +0100

    Create Maze_interactive_Solution.py

---
+++ Day 6- while loop/Maze_interactive_Solution.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 29 16:08:43 2025
+
+@author: toora
+"""
+# =============================================================================
+# Website is given here
+# =============================================================================
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# =============================================================================
+# Solution
+# =============================================================================
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    for i in range (3):
+        if at_goal():
+            done()
+        elif right_is_clear():    
+             turn_right()
+             move()   
+        elif wall_in_front():
+             turn_left()
+
+        elif wall_on_right():
+             move()
+        else:
+            move()
+        
+    
+    
+ 
