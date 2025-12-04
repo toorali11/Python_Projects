b61da6e4ffc86b7cc2d8b192653af8c031836227
Toor Ali <toorali11@gmail.com>
2025-12-04 21:07:21 +0100

    Create Nesting dictionary.py

---
+++ Day 9-Dictionaries and nesting/Nesting dictionary.py
@@ -0,0 +1,12 @@
+travel_log={
+    "france":{
+       "kitnay": ["wuhu", "paris","mazay"],
+        "numbervisited":9,
+        "aha":87
+    },
+    "germany":["fklads", "ijdfsoa"]
+}
+
+a=travel_log["france"]["kitnay"][1]
+print(a)
+
