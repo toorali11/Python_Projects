b4b77f68857a5ae5228c7146240d852a583291c0
Toor Ali <toorali11@gmail.com>
2025-12-05 23:32:18 +0100

    Delete Important example to visualize 2 lists output.py

--- Day 4- Randomisation and lists/Important example to visualize 2 lists output.py
+++
@@ -1,6 +0,0 @@
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
- 
-dirty_dozen = [fruits, vegetables]
- 
-print(dirty_dozen[0][1]) #index 0 from dirty_dozen and index 1 from that index 0 i.e. from fruits.
