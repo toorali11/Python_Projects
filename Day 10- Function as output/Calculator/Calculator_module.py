f606b79f2a2544ae089ab7107e50be2997de5353
Toor Ali <toorali11@gmail.com>
2025-12-06 18:25:59 +0100

    Create Calculator_module.py

---
+++ Day 10- Function as output/Calculator/Calculator_module.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  6 16:31:14 2025
+
+@author: toora
+"""
+
+def addition(n1,n2):
+    return n1+n2
+
+def subtraction(n1,n2):
+    return n1-n2
+
+def multiplication(n1,n2):
+    return n1*n2
+
+def division(n1,n2):
+    return n1/n2
+
