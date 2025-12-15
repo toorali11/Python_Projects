d8c13bf63c80f18fddd003f67aa321722216e87d
Toor Ali <toorali11@gmail.com>
2025-12-15 23:05:15 +0100

    Create Class Turtle.py

---
+++ Day 15- Object Oriented Programming/Class Turtle.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 15 20:23:33 2025
+
+@author: toora
+"""
+# ímport module
+import turtle
+
+# Testing Turtle class to get to know about i more
+
+Timmy=turtle.Turtle()
+Timmy.shape("turtle")       # shape formation
+Timmy.color("green")        # Color for that shape
+print(Timmy)
+Timmy.forward(100)          # moving forward command
+
+
+my_screen=turtle.Screen()   # Screen with specific height
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
