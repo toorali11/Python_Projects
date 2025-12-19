6e5d9a6a89a10ab6eca36934f4dbcdb75d13c91c
Toor Ali <toorali11@gmail.com>
2025-12-19 04:27:36 +0100

    Create question_model.py

---
+++ Day 16- Making Blueprints-The Class/Quiz Game/question_model.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 19 03:40:26 2025
+
+@author: toora
+"""
+
+class Question:
+    
+    def __init__(self, text, answer):
+        self.text= text
+        self.answer= answer
+        
